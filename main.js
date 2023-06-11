03aed4624ac781cfacb33c30e664db8dfdbf882d
Mssjim <mrmssjim@gmail.com>
2023-06-11 16:51:30 -0300

    Add: core

---
+++ main.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+
+// Code
+console.log('Starting...');
+console.log(`\x1b[36m====================================\x1b[0m`);
+
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.MessageContent
+  ]});
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = new Discord.Collection();
+
+// Handlers
+fs.readdirSync('./src/handlers/').forEach(async(handler) => {
+    await require(`./src/handlers/${handler}`)(client);
+});
+
+client.login(process.env.TOKEN);
