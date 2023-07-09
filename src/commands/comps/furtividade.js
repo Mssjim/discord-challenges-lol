428da02a9f9742e985ee6e07f9ad7f188ee2397f
Mssjim <mrmssjim@gmail.com>
2023-07-09 01:38:37 -0300

    Feat: show 'regions'

--- src/commands/comps/furtividade.js
+++ src/commands/comps/furtividade.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder } = require("discord.js");
 const colors = require("../../resources/colors.json");
 const images = require("../../resources/images.json");
+const data = require("../../resources/champions.json");
 
 module.exports = {
     name: 'furtividade',
@@ -15,7 +16,19 @@ module.exports = {
             .setColor(colors.primary)
             .setImage(images.furtividade)
             .setTimestamp()
-            .setFooter({text: client.user.username, iconURL: client.user.displayAvatarURL()})
+            .setFooter({text: client.user.username, iconURL: client.user.displayAvatarURL()});
+
+        const comp = data.comps['Furtividade'];
+        const champs = comp.champs;
+        for(regionName in data.regioes) {
+            const region = data.regioes[regionName];
+            const similarChamps = region.filter(champ => champs.includes(champ));
+            if(similarChamps.length >= comp.min) {
+                embed.addFields([{
+                    name: regionName, value: "`" + similarChamps.join("` `") + "`", inline: true
+                }])
+            }
+        }
         
         msg.channel.send({embeds: [embed], content: msg.author.toString()});
     }
--- src/commands/comps/terreno.js
+++ src/commands/comps/terreno.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder } = require("discord.js");
 const colors = require("../../resources/colors.json");
 const images = require("../../resources/images.json");
+const data = require("../../resources/champions.json");
 
 module.exports = {
     name: 'terreno',
@@ -15,7 +16,19 @@ module.exports = {
             .setColor(colors.primary)
             .setImage(images.terreno)
             .setTimestamp()
-            .setFooter({text: client.user.username, iconURL: client.user.displayAvatarURL()})
+            .setFooter({text: client.user.username, iconURL: client.user.displayAvatarURL()});
+
+        const comp = data.comps['Terreno'];
+        const champs = comp.champs;
+        for(regionName in data.regioes) {
+            const region = data.regioes[regionName];
+            const similarChamps = region.filter(champ => champs.includes(champ));
+            if(similarChamps.length >= comp.min) {
+                embed.addFields([{
+                    name: regionName, value: "`" + similarChamps.join("` `") + "`", inline: true
+                }])
+            }
+        }
         
         msg.channel.send({embeds: [embed], content: msg.author.toString()});
     }
